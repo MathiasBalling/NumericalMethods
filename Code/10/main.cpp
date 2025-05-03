87577310bf69f00a86d8c9bb6343ca08fd3574c6
Mathias Balling <math260r@gmail.com>
2025-05-03 11:45:08 +0200

    Printing of ode done

--- Code/10/main.cpp
+++ Code/10/main.cpp
@@ -13,18 +13,19 @@ VecDoub derivs(const Doub x, VecDoub_I &y) {
 }
 
 int main() {
+
   {
     std::println("\n1nd order (Euler method):");
     double vals[2] = {1.0, 1.0};
     const VecDoub y(2, vals);
     double low = 0.0;
     double high = 10.0;
-    std::vector<VecDoub> result;
-    for (int n = 5; n <= 1000; n *= 2) {
-      auto y_n = y;
-      y_n = first_order_runge_kuttea_method(low, high, n, y_n, derivs);
-      util::print(y_n, std::format("1st order with h={}", (high - low) / n));
-    }
+    print_ode_table(low, high, 5, y, derivs, ODEMethod::Euler, 1e-5);
+    // for (int n = 5; n <= 1000; n *= 2) {
+    //   auto y_n = y;
+    //   y_n = first_order_runge_kuttea_method(low, high, n, y_n, derivs);
+    //   util::print(y_n, std::format("1st order with h={}", (high - low) / n));
+    // }
   }
 
   {
@@ -33,13 +34,14 @@ int main() {
     const VecDoub y(2, vals);
     double low = 0.0;
     double high = 10.0;
-    std::vector<VecDoub> result;
-    for (int n = 5; n <= 1000; n *= 2) {
-      const auto res =
-          second_order_runge_kuttea_method(low, high, n, y, derivs);
-      util::print(res,
-                  std::format("2st order with h={}", (high - low) / (double)n));
-    }
+    print_ode_table(low, high, 5, y, derivs, ODEMethod::Midpoint, 1e-5);
+    // for (int n = 5; n <= 1000; n *= 2) {
+    //   const auto res =
+    //       second_order_runge_kuttea_method(low, high, n, y, derivs);
+    //   util::print(res,
+    //               std::format("2st order with h={}", (high - low) /
+    //               (double)n));
+    // }
   }
 
   {
@@ -48,13 +50,14 @@ int main() {
     const VecDoub y(2, vals);
     double low = 0.0;
     double high = 10.0;
-    std::vector<VecDoub> result;
-    for (int n = 5; n <= 1000; n *= 2) {
-      const auto res =
-          fourth_order_runge_kuttea_method(low, high, n, y, derivs);
-      util::print(res,
-                  std::format("4st order with h={}", (high - low) / (double)n));
-    }
+    print_ode_table(low, high, 5, y, derivs, ODEMethod::FORTH_ORDER, 1e-5);
+    // for (int n = 5; n <= 1000; n *= 2) {
+    //   const auto res =
+    //       fourth_order_runge_kuttea_method(low, high, n, y, derivs);
+    //   util::print(res,
+    //               std::format("4st order with h={}", (high - low) /
+    //               (double)n));
+    // }
   }
 
   {
@@ -63,27 +66,30 @@ int main() {
     const VecDoub y(2, vals);
     double low = 0.0;
     double high = 10.0;
-    std::vector<VecDoub> result;
-    for (int n = 5; n <= 1000; n *= 2) {
-      auto y_n = y;
-      VecDoub res = trapezoidal(low, high, n, y_n, derivs);
-      util::print(
-          res, std::format("Trapezoidal with h={}", (high - low) / (double)n));
-    }
+    print_ode_table(low, high, 5, y, derivs, ODEMethod::TRAPEZOIDAL, 1e-5);
+    // for (int n = 5; n <= 1000; n *= 2) {
+    //   auto y_n = y;
+    //   VecDoub res = trapezoidal(low, high, n, y_n, derivs);
+    //   util::print(
+    //       res, std::format("Trapezoidal with h={}", (high - low) /
+    //       (double)n));
+    // }
   }
 
   {
+    // INFO: Very unstable method...
     std::println("\nLeap Frog method:");
     double vals[2] = {1.0, 1.0};
     const VecDoub y(2, vals);
     double low = 0.0;
     double high = 10.0;
-    std::vector<VecDoub> result;
-    for (int n = 5; n <= 1000; n *= 2) {
-      auto y_n = y;
-      VecDoub res = leap_frog_method(low, high, n, y_n, derivs);
-      util::print(res,
-                  std::format("Leap Frog with h={}", (high - low) / (double)n));
-    }
+    print_ode_table(low, high, 5, y, derivs, ODEMethod::LEAP_FROG, 1e-5);
+    // for (int n = 5; n <= 1000; n *= 2) {
+    //   auto y_n = y;
+    //   VecDoub res = leap_frog_method(low, high, n, y_n, derivs);
+    //   util::print(res,
+    //               std::format("Leap Frog with h={}", (high - low) /
+    //               (double)n));
+    // }
   }
 }
--- Code/utils/ode.h
+++ Code/utils/ode.h
@@ -5,6 +5,10 @@
 #include "nr3.h"
 #include "utilities.h"
 
+enum class ODEMethod { Euler, Midpoint, FORTH_ORDER, TRAPEZOIDAL, LEAP_FROG };
+
+// Too keep track of f-comps
+static size_t f_comps_current = 0;
 // Euler method
 VecDoub
 first_order_runge_kuttea_method(double low, double high, int steps, VecDoub_I y,
@@ -13,6 +17,7 @@ first_order_runge_kuttea_method(double low, double high, int steps, VecDoub_I y,
 
   VecDoub y_n = y;
   for (double x = low; x < high; x += h) {
+    f_comps_current++;
     auto y_n_next = y_n + h * derivs(x, y_n);
     y_n = y_n_next;
   }
@@ -27,6 +32,7 @@ VecDoub second_order_runge_kuttea_method(double low, double high, int steps,
   const double h = (high - low) / (double)steps;
   VecDoub y_n = y;
   for (double x_n = low; x_n < high; x_n += h) {
+    f_comps_current += 2;
     auto k1 = h * derivs(x_n, y_n);
     auto k2 = h * derivs(x_n + 0.5 * h, y_n + 0.5 * k1);
     auto y_n_next = y_n + k2;
@@ -42,6 +48,7 @@ VecDoub fourth_order_runge_kuttea_method(double low, double high, int steps,
   const double h = (high - low) / (double)steps;
   VecDoub y_n = y;
   for (double x_n = low; x_n < high; x_n += h) {
+    f_comps_current += 4;
     auto k1 = h * derivs(x_n, y_n);
     auto k2 = h * derivs(x_n + 0.5 * h, y_n + 0.5 * k1);
     auto k3 = h * derivs(x_n + 0.5 * h, y_n + 0.5 * k2);
@@ -59,9 +66,11 @@ VecDoub trapezoidal(double x_low, double x_high, int steps, VecDoub_I &y,
   VecDoub y_n = y;
   for (double x_n = x_low; x_n < x_high; x_n += h) {
     // Euler step
+    f_comps_current += 1;
     auto y_n_next = y_n + h * derivs(x_n, y_n);
 
-    auto func = [=](VecDoub y_guess) {
+    auto func = [&](VecDoub y_guess) {
+      f_comps_current += 2;
       return y_guess - y_n -
              h / 2 * (derivs(x_n, y_n) + derivs(x_n + h, y_guess));
     };
@@ -77,13 +86,202 @@ VecDoub trapezoidal(double x_low, double x_high, int steps, VecDoub_I &y,
 VecDoub leap_frog_method(double low, double high, int steps, VecDoub_I &y,
                          VecDoub derivs(const Doub x, VecDoub_I &y)) {
   const double h = (high - low) / (double)steps;
+  f_comps_current += 2;
   VecDoub y_n_last = y + h * derivs(low, y);
   VecDoub y_n = y_n_last + h * derivs(low + h, y_n_last);
   for (double x_n = low + h + h; x_n < high; x_n += h) {
+    f_comps_current++;
     VecDoub y_n_next = y_n_last + 2 * h * derivs(x_n, y_n);
     y_n_last = y_n;
     y_n = y_n_next;
   }
   return y_n;
 }
+
+std::vector<VecDoub> alpha_k_order_computed(const std::vector<VecDoub> &A_k) {
+  const auto vec_size = A_k[0].size();
+  std::vector<VecDoub> alpha_k = {
+      VecDoub(vec_size, NAN),
+      VecDoub(vec_size, NAN)}; // No error on the first two
+
+  for (size_t i = 2; i < A_k.size(); i++) {
+    VecDoub alpha_i(vec_size);
+    for (size_t j = 0; j < vec_size; j++) {
+      const double A_1 = A_k[i - 2][j];
+      const double A_2 = A_k[i - 1][j];
+      const double A_3 = A_k[i][j];
+      alpha_i[j] = (A_1 - A_2) / (A_2 - A_3);
+    }
+    alpha_k.push_back(alpha_i);
+  }
+  return alpha_k;
+}
+
+std::vector<VecDoub>
+richardson_extrapolation_error(const std::vector<VecDoub> &A_k,
+                               const double alpha_k_order_expected) {
+  const auto vec_size = A_k[0].size();
+  std::vector<VecDoub> A_R = {VecDoub(vec_size, NAN)}; // No error on the first
+  for (size_t i = 1; i < A_k.size(); i++) {
+    VecDoub error(vec_size);
+    for (size_t j = 0; j < vec_size; j++) {
+      const double A_1 = A_k[i - 1][j];
+      const double A_2 = A_k[i][j];
+      error[j] = (A_2 - A_1) / (alpha_k_order_expected - 1);
+    }
+    A_R.push_back(error);
+  }
+  return A_R;
+}
+
+VecDoub
+richardson_extrapolation_error_current(const std::vector<VecDoub> &A_k,
+                                       const double alpha_k_order_expected) {
+  VecDoub errors = VecDoub(A_k[0].size(), std::numeric_limits<double>::max());
+
+  for (size_t i = 0; i < A_k[0].size(); i++) {
+    const double A_1 = A_k[A_k.size() - 2][i];
+    const double A_2 = A_k[A_k.size() - 1][i];
+    // FIX: Only use expected if compute_order_estimate is good.
+    errors[i] = (A_2 - A_1) / (alpha_k_order_expected - 1);
+  }
+  return errors;
+}
+
+std::vector<VecDoub>
+compute_order_estimate(const std::vector<VecDoub> &alpha_k_computed) {
+  const auto vec_size = alpha_k_computed[0].size();
+  std::vector<VecDoub> order_estimate = {
+      VecDoub(vec_size, NAN),
+      VecDoub(vec_size, NAN)}; // No order for first two entries
+  for (size_t i = 2; i < alpha_k_computed.size(); i++) {
+    VecDoub p(vec_size);
+    for (size_t j = 0; j < vec_size; j++) {
+      p[j] = log2(abs(alpha_k_computed[i][j]));
+    }
+    order_estimate.push_back(p);
+  }
+  return order_estimate;
+}
+
+void print_ode_table(const double low, const double high,
+                     const int starting_steps, VecDoub_I &y,
+                     VecDoub derivs(const Doub x, VecDoub_I &y),
+                     const ODEMethod type, const double accuracy = 1e-5,
+                     const size_t max_steps = 20000) {
+  std::vector<VecDoub> A_k;
+  std::vector<int> f_comps;
+  double expected_order = 0.0;
+  int its = starting_steps;
+  bool should_stop = false;
+  while (!should_stop) {
+    f_comps_current = 0;
+    auto y_n = y;
+    switch (type) {
+    case ODEMethod::Euler:
+      A_k.push_back(
+          first_order_runge_kuttea_method(low, high, its, y_n, derivs));
+      f_comps.push_back(f_comps_current);
+      expected_order = 1.0;
+      break;
+    case ODEMethod::Midpoint:
+      A_k.push_back(
+          second_order_runge_kuttea_method(low, high, its, y_n, derivs));
+      f_comps.push_back(f_comps_current);
+      expected_order = 2.0;
+      break;
+    case ODEMethod::FORTH_ORDER:
+      A_k.push_back(
+          fourth_order_runge_kuttea_method(low, high, its, y_n, derivs));
+      f_comps.push_back(f_comps_current);
+      expected_order = 4.0;
+      break;
+    case ODEMethod::TRAPEZOIDAL:
+      A_k.push_back(trapezoidal(low, high, its, y_n, derivs));
+      f_comps.push_back(f_comps_current);
+      expected_order = 2.0;
+      break;
+    case ODEMethod::LEAP_FROG:
+      A_k.push_back(leap_frog_method(low, high, its, y_n, derivs));
+      f_comps.push_back(f_comps_current);
+      expected_order = 2.0;
+      break;
+    default:
+      throw("Unknown integration type");
+    }
+
+    if (A_k.size() >= 2) {
+      const auto errors =
+          richardson_extrapolation_error_current(A_k, pow(2, expected_order));
+      double biggest_error = 0.0;
+      for (size_t i = 0; i < errors.size(); i++) {
+        if (abs(errors[i]) > biggest_error) {
+          biggest_error = abs(errors[i]);
+        }
+      }
+      if (biggest_error < accuracy) {
+        should_stop = true;
+      } else if (its > max_steps) {
+        should_stop = true;
+      }
+    }
+    its *= 2.0;
+  }
+
+  const auto vec_size = y.size();
+
+  // Calculate the differences
+  std::vector<VecDoub> A_diff_k = {VecDoub(vec_size, NAN)};
+  for (size_t i = 1; i < A_k.size(); i++) {
+    VecDoub diff(vec_size);
+    for (size_t j = 0; j < vec_size; j++) {
+      diff[j] = A_k[i - 1][j] - A_k[i][j];
+    }
+    A_diff_k.push_back(diff);
+  }
+
+  // Calculate the orders
+  auto alpha_k_computed = alpha_k_order_computed(A_k);
+
+  // Calculate the richardson extrapolation
+  auto rich_error = richardson_extrapolation_error(
+      A_k, pow(2, expected_order)); // pow(2, expected_order) as we use
+                                    // N-1=1,2,4,8
+
+  auto order_estimate = compute_order_estimate(alpha_k_computed);
+
+  // Print the table
+  for (size_t i = 0; i < vec_size; i++) {
+    // Table header
+    std::println("For y({})", i);
+    std::println("|{:^7}|{:^21}|{:^21}|{:^21}|{:^21}|{:^21}|{:^10}|", "i",
+                 "A(i)", "A(i-1)-A(i)", "alpha^k", "Rich error", "Order est.",
+                 "f comps");
+    // Table header separator
+    std::println("|{:-^7}|{:-^21}|{:-^21}|{:-^21}|{:-^21}|{:-^21}|{:-^10}|", "",
+                 "", "", "", "", "", "");
+
+    // Table body
+    int its = starting_steps;
+    for (size_t j = 0; j < A_k.size(); j++) {
+      if (j == 0) {
+        std::println(
+            "|{:^7}|{:^21.12}|{:^21.12}|{:^21.12}|{:^21.12}|{:^21.12}|{:^10}|",
+            its, A_k[j][i], "", "", "", "", f_comps.at(j));
+      } else if (j == 1) {
+        std::println(
+            "|{:^7}|{:^21.12}|{:^21.12}|{:^21.12}|{:^21.12}|{:^21.12}|{:^10}|",
+            its, A_k[j][i], A_diff_k[j][i], "", rich_error[j][i], "",
+            f_comps.at(j));
+      } else {
+        std::println(
+            "|{:^7}|{:^21.12}|{:^21.12}|{:^21.12}|{:^21.12}|{:^21.12}|{:^10}|",
+            its, A_k[j][i], A_diff_k[j][i], alpha_k_computed[j][i],
+            rich_error[j][i], order_estimate[j][i], f_comps[j]);
+      }
+      its *= 2;
+    }
+  }
+}
+
 #endif // !ODE_H
