458d3f9280445fe0ce9e2c84f805a61d218cd03c
Mathias Balling <math260r@gmail.com>
2025-05-16 14:30:18 +0200

    Restructure + exam ready

---
+++ exam/main5.cpp
@@ -0,0 +1,7 @@
+#include "nr3.h"
+#include <print>
+
+int main() {
+  std::println("Exam exercise 5");
+  return 0;
+}
---
+++ exam/main6.cpp
@@ -0,0 +1,7 @@
+#include "nr3.h"
+#include <print>
+
+int main() {
+  std::println("Exam exercise 6");
+  return 0;
+}
